7ddd2fabef85f1923a7c92e075ac2e651d8a04c8
Mubashir Hanif <mubashir@noyes-tech.com>
2023-06-22 00:05:05 +0200

    update the experience.

--- src/components/home/Experience.jsx
+++ src/components/home/Experience.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Boeing from "../../editable-stuff/boeing.jpg";
-import Dell from "../../editable-stuff/dell-logo.jpg"
+import noyes from "../../editable-stuff/noyes_logo.png";
+import folio3 from "../../editable-stuff/folio3_logo.png";
+import BMW from "../../editable-stuff/BMW_logo.png"
 const divStyle = {
     display: 'flex',
     flexDirection: 'row',
@@ -18,36 +19,53 @@ return (
                 <div className="col text-center" >
                     <img
                         // className="border border-secondary rounded-circle"
-                        src={Dell}
-                        alt="dell technologies logo"
-                        width="420"
-                        height="70"
+                        src={noyes}
+                        alt="Noyes Technologies Logo"
+                        width="250"
+                        height="100"
                     />
                     <br/>
                     <br/>
                     <p className="lead text-center" style={{fontSize:'22px'}}>
-                        Software Engineer Intern  
+                        Senior Software Engineer
                         <br/>
-                        June - July 2020
+                        April 2021 - Present
                     </p>
-                    {/* <h3>Dell Technologies</h3> */}
+                    {/* <h3>BMW Technologies</h3> */}
+                </div>
+                <div className="col text-center" >
+                    <img
+                        // className="border border-secondary rounded-circle"
+                        src={BMW}
+                        alt="BMW AG Logo"
+                        width="100"
+                        height="100"
+                    />
+                    <br/>
+                    <br/>
+                    <p className="lead text-center" style={{fontSize:'22px'}}>
+                        Computer Vision Research Intern
+                        <br/>
+                        July 2019 - July 2020
+                    </p>
+                    {/* <h3>BMW Technologies</h3> */}
                 </div>
                 <div className="col text-center">
                     <img
                         // className="border border-secondary rounded-circle"
-                        src={Boeing}
-                        alt="boeing logo"
-                        width="300"
-                        height="80"
+                        src={folio3}
+                        alt="folio3 logo"
+                        width="250"
+                        height="100"
                     />
                     <br/>
                     <br/>
                     <p className="lead text-center" style={{fontSize:'22px'}}>
-                        Software Engineer Intern  
+                        Software Engineer  
                         <br/>
-                        May - August 2019
+                        May 2015 - August 2016
                     </p>
-                    {/* <h3>Boeing</h3> */}
+                    {/* <h3>folio3</h3> */}
                 </div>
             </div>
         </div>
